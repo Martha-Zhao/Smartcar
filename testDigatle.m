3354a6d1d81b050505d5cfa95ada058193779f87
Martha-Zhao <34574457+Martha-Zhao@users.noreply.github.com>
2018-03-11 22:49:44 +0800

    更新了数字切割方案
    ①对原始图像进行颜色反转，减轻数字运算量
       但是为了显示效果，切割的是临时图片数组，还需要修改
    ②切割方案从找棋盘格边界变成找数字上下边界，目前寻找的是左上右下，根据数字长宽比加偏移量
       但是存在切割完的数块大小不一致，需要及归一化修改
       切割阈值需要进一步修改，确保切割的准确性
       可以尝试先通过棋盘格边界确定单独数字，再对单个数字进行切割处理，防止同行同列数字对其造成影响
    ③根据面积进行数字识别，目前状况并没有想象中完美，需要进行归一化后重新识别
       进行面积处理的数字块需要从0-1二值化图像中切割------目前采用的是pic_tem图像切割
    ④面积识别不能通过的6-9可采用重心方式识别

--- testDigatle.m
+++ testDigatle.m
@@ -1,70 +1,78 @@
 clc;
 clear;
-pic = imread('dig.gif'); %??????
+pic = imread('dig.gif'); %导入原始图像
 figure(1);
 imshow (pic)
-title("original picture") %??????
+title("original picture") %显示原始图像
 
-%%  Binarization    ???
+%%  Binarization   
 pic_bin = pic;  
 pic_tem = pic;
 for i = 1:size(pic,1)
     for j = 1:size(pic,2)
         if pic(i,j) > 100
-            pic_bin(i,j) = 1;  %%?????????
-            pic_tem(i,j) = 255; %%????
+            pic_bin(i,j) = 0;  %二值化,黑白反转图像
+            pic_tem(i,j) = 1;%显示二值化并反转结果
         else
-            pic_bin(i,j) = 0;
-            pic_tem(i,j) = 0;
+            pic_bin(i,j) = 1;%反转图像
+            pic_tem(i,j) = 255;%显示二值化并反转结果
         end
     end
 end
 
-% figure(2);
-% imshow(pic_tem)
-% title("Binarization")
+figure(2);
+imshow(pic_tem)
+title("Binarization")
 
-%%  Cut    ??
-pic_sum_collon = sum(pic_bin,2);%??????
-pic_sum_row = sum(pic_bin);     %??????
-% figure(3);
-% plot(pic_sum_collon,'r');
-% title("sum_collon")
-% hold on;
-% plot(pic_sum_row,'b')
-num_collon = 0;  %?????
-num_row    = 0;  %?????
-loc        = zeros(20,2);  %????????????????????
-for i = 1:size(pic_sum_collon,1)-1  %???
-    if (pic_sum_collon(i)<100) && (pic_sum_collon(i+1)>500)  %????
-        num_collon = num_collon + 1; %???????????
-        loc(num_collon,1) = i; %???????
+%%  Cut   
+pic_sum_collon = sum(pic_bin,2);%行和数组
+pic_sum_row = sum(pic_bin);     %列和数组
+
+num_collon = 0;  %行跳变数  ----期待值为18
+num_row    = 0;  %列跳变数
+loc        = zeros(20,2);  %跳变位置数组，第一列为行数第二列为列数
+
+for i = 1:size(pic_sum_collon,1)-1  %行遍历
+    if (pic_sum_collon(i)<50) && (pic_sum_collon(i+1)>50) && (pic_sum_collon(i+1)<600)  %当前为黑，下一行有白 且下一行非全白
+        num_collon = num_collon + 1; %行数加一  
+        loc(num_collon,1) = i+1; %记录行跳变位置
     end
-    if pic_sum_collon(i) >500  %????????
-       loc(num_collon+1,1) = i; %??????
-    end
-    if (pic_sum_row(i)==0) && (pic_sum_row(i+1)>500)  %?????
-        num_row = num_row +1; %???????????
-        loc(num_row,2) = i;  %?????
-    end
-    if pic_sum_row(i) > 500  %????????
-       loc(num_row+1,2) = i;  %????????
+
+    if (pic_sum_row(i)>50) && (pic_sum_row(i+1)<51) && (pic_sum_row(i)<600) %当前为黑，下一行有白 且下一行非全白
+        num_row = num_row +1; %列数加一
+        loc(num_row,2) = i;  %记录列跳变数
     end
 end
-num_collon = num_collon+1;  %?????
-num_row    = num_row+1;     %?????
 
-for i = 1:num_collon  %?????
-    eval(['pic_sub',num2str(i),'=pic(loc(i,1)+1:loc(i+1,1)-2,loc(1,2)+1:loc(2,2)-2);']);
-end
+% for i = 1:num_collon  %
+%     eval(['pic_sub',num2str(i),'=pic_tem(loc(i,1):loc(i,2)+8,loc(i,1)+5:loc(i,2));']);%数字并非正方形因此需要调整
+% end
+pic_sub0 = pic_tem( loc(2,1) : loc(2,2)+8 , loc(1,1)+5 : loc(1,2) );
+pic_sub1 = pic_tem( loc(2,1) : loc(2,2)+8 , loc(2,1)+5 : loc(2,2) );
+pic_sub2 = pic_tem( loc(1,1) : loc(1,2)+8 , loc(6,1)+5 : loc(6,2) );
+pic_sub3 = pic_tem( loc(1,1) : loc(1,2)+8 , loc(8,1)+5 : loc(8,2) );
+pic_sub4 = pic_tem( loc(2,1) : loc(2,2)+8 , loc(3,1)+5 : loc(3,2) );
+pic_sub5 = pic_tem( loc(2,1) : loc(2,2)+8 , loc(8,1)+5 : loc(8,2) );
+pic_sub6 = pic_tem( loc(2,1) : loc(2,2)+8 , loc(4,1)+5 : loc(4,2) );
+pic_sub7 = pic_tem( loc(2,1) : loc(2,2)+8 , loc(6,1)+5 : loc(6,2) );
+pic_sub8 = pic_tem( loc(2,1) : loc(2,2)+8 , loc(7,1)+5 : loc(7,2) );
+pic_sub9 = pic_tem( loc(2,1) : loc(2,2)+8 , loc(9,1)+5 : loc(9,2) );
 
-figure(4);  %??????
-for i = 1:4
-    subplot(2,2,i);
-    eval(['imshow(','pic_sub',num2str(i),')']);
-end
+figure(3);  %显示切割好的图像
 title("pic sub")
+for i = 1:10    
+    subplot(2,5,i);
+    eval(['imshow(','pic_sub',num2str(i-1),')']);
+end
 
-%% ??
+
+%% 放缩图像
+
+%% 根据面积识别数字
+for i = 0:9
+    eval(['pic_sum',num2str(i),'=sum(sum(pic_sub',num2str(i),'));']);
+end
+
+% pic_sum = sum(sum(pic_sub1));
 
 
