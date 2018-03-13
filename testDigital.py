492671ffe09e8d778ec103ba54d21fe0facb2ec0
Martha-Zhao <34574457+Martha-Zhao@users.noreply.github.com>
2018-03-13 11:29:05 +0800

    python - opencv
    python 实现原始图像显示

---
+++ testDigital.py
@@ -0,0 +1,17 @@
+
+import cv2
+
+# Load original picture and show 
+Img_Original = cv2.imread('dig.jpg')
+
+cv2.namedWindow('Original Picture')
+cv2.imshow('Original Picture',Img_Original)
+cv2.waitKey(0)
+cv2.destroyWindow('Original Picture')
+
+# invert Original picture 
+
+
+
+
+
