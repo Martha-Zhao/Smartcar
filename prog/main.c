35c46d20550642b2627408e271b2ee5f4da83ad0
Martha-Zhao <34574457+Martha-Zhao@users.noreply.github.com>
2018-03-18 22:55:48 +0800

    Eight Queen
    C完成8皇后约束问题。仅依靠约束条件遍历，没有使用其他优化算法。
    遍历是通过检测当前行的每一个位置是否可以安全放置皇后，
        如果可以则放置，判断当前是否为第八行。
               如果是第八行，结束，
               否则进行下一行。
      否则进行下一个位置检测。
    如果进行到第8个位置仍无法放置，退回上一行重新选择。
    
    当前程序没有使用进一步的优化算法，仅仅通过普通遍历完成。

---
+++ prog/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static int gEightQueen[8] = {0};
+static int gcount = 0;
+
+void print()
+{
+    int outer;
+    int inner;
+
+    for (outer = 0; outer < 8;outer++)
+    {
+        for (inner = 0; inner <gEightQueen[outer] ; inner++ )
+            printf("* ");
+
+        printf ("# ");
+
+        for (inner = gEightQueen[outer]+1; inner < 8; inner++)
+            printf ("* ");
+
+        printf("    %d \n" , gEightQueen[outer]);
+    }
+
+    printf("-------------------------------------\n");
+
+
+
+
+}
+
+int check (int loop ,int value)
+{
+    int index;
+    int data;
+
+    for (index = 0; index < loop ; index++)
+    {
+        data = gEightQueen[index];
+
+        if ( (index == loop) || (data == value) )
+            return 0;
+
+        if ( ( (index + data) == (loop + value) ) ||( (index - data) == (loop - value) ) )
+            return 0;
+    }
+
+    return 1;
+}
+
+
+void eight_queen (int index)
+{
+    int loop;
+
+    for (loop = 0; loop < 8; loop++)
+    {
+        if  (check (index,loop) )
+        {
+            gEightQueen[index] = loop;
+
+            if (index == 7)
+            {
+                gcount++, print();
+                gEightQueen[index] = 0;
+                return ;
+            }
+
+            eight_queen(index + 1);
+            gEightQueen[index] = 0;
+        }
+    }
+}
+
+
+
+int main(int argc, char* argv[])
+{
+    int a = 0;
+    int b = 1;
+    int c = 2;
+
+    c = a + b;
+    a = c + b;
+    b = a + c;
+    eight_queen(0);
+    printf("total = %d \n",gcount);
+    return 1;
+}
