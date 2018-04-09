a8b2ac48997653b0a6015d2ef5a8d4096b67cb60
Martha-Zhao <34574457+Martha-Zhao@users.noreply.github.com>
2018-04-09 11:10:58 +0800

    0 最多的数组整理完成
    找到了若干0 最多的ans[i]
    输出去掉0 后的loc[] and ans[i]

--- prog/location/main_2_3.c
+++ prog/location/main_2_3.c
@@ -110,18 +110,25 @@ int main()
 
     /**********随机生成棋子位置************/
     int *loc_p;
-    int cun = 0;
+    int *loc_q;
+    int *ans_p;
+
+    int temp_sort = 0;
 
     /**********match zeros*****************/
-    int max_zeros = 0;
-    int *ans_row_p;
-    int *ans_col_p;
-    int temp_count = 0;
     int k;
 
+    int count_zeros [92][16] = {0};
+    int *count_zeros_p;
+
+    int loc_changed[][8] = {0};
+    int ans_changed[][8] = {0};
+    int optimal_solution_count = 0;
+
+    int max_zeros = 0;
 
     /******************per edition*************************/
-    // random location
+     //random location
     srand((unsigned)time(0)); //随机种子
 
     printf("random location is \n");
@@ -130,19 +137,18 @@ int main()
     for (; loc_p <= &loc[7]; loc_p++)
     {
         *loc_p = rand()%63+1;
-        cun++;
         printf("%d ",*loc_p);
         // test whether repeated
-        for (int j = 0; j < cun-1; j++)
+        for (loc_q = loc; loc_q < loc_p; loc_q++)
         {
-            if (*loc_p == loc[j]) //repeated
+            if (*loc_p == *loc_q) //repeated
             {
                 // print random location
                 if (*loc_p > 9)
                     printf("\b\b\b");
                 else
                     printf("\b\b");
-                cun--;
+
                 loc_p--;
                 break;
             }
@@ -150,57 +156,52 @@ int main()
     }
     printf("\n");
 
-    // sort
+     //sort
     printf("\nlocation sorted\n");
-    for (int i = 0; i<8; i++)
+
+    for ( loc_p = loc; loc_p <= &loc[7]; loc_p ++ )
     {
-        for (int j=i+1;j<8;j++)
+        for ( loc_q = loc_p+1; loc_q <= &loc[7];loc_q++ )
         {
-            if (loc[j] < loc[i])
+            if ( *loc_q < *loc_p )
             {
-                temp_sort = loc[j];
-                loc[j] = loc[i];
-                loc[i] = temp_sort;
+                temp_sort = *loc_q;
+                *loc_q = *loc_p;
+                *loc_p = temp_sort;
             }
         }
-        printf("%d ",loc[i]);
+        printf("%d ",*loc_p);
     }
 
-    //print all answers
+
+    ///print all answers
     printf("\nAll answers are\n");
 
-    // change the form of ans[][]
-    for (int i = 0; i < 92 ;i++)
+     //change the form of ans[][]
+    for ( int i = 0; i <92; i++ )
     {
-        for (int j = 0; j<8; j++)
+        for ( int j = 0; j < 8; j++)
         {
-            ans[i][j] = j * 8 + ans[i][j];
-            //printf("%d  ",ans[i][j]);
+            *(*(ans+i)+j) += j * 8;
+           // printf("%d ",*(*(ans+i)+j) );
         }
         //printf("\n");
     }
 ///****************************match zeros****************************************/
     //count zeros
-    printf("\ncount_zeros is\n");
-
-
-
     for (int i = 0; i < 92; i++)//92种解遍历
     {
         k=0;
-        for (int j=0; j < cun;j++)//行遍历
+        count_zeros_p = count_zeros[i];
+        for (int j=0; j < 8;j++)//行遍历
         {
-            temp_count = 0;
             for (;k <8;)
             {
                 if (loc[j] == ans[i][k]) //匹配到0
                 {
-                    count_zeros[i][0]++;//0计数++
-                    temp_count = count_zeros[i][0];
-                    count_zeros[i][2*temp_count-1] = j;
-                    count_zeros[i][2*temp_count] = k;
-//                    count_zeros[i][&(count_zeros[i][0])] = j;
-//                    count_zeros[i][&(count_zeros[i][0])+1] = k;
+                    count_zeros[i][0] +=1;
+                    *++count_zeros_p = j;
+                    *++count_zeros_p = k;
                     k++;//匹配下一个位置
                     break;
                 }
@@ -217,82 +218,73 @@ int main()
         if (max_zeros < count_zeros[i][0]) //寻找0最多的个数
             max_zeros = count_zeros[i][0];
     }
-//    printf("\n max_zeros = %d \n",max_zeros);
-//    /******optimal solution round one************/
-//    printf("\nzeros matched\n");
-//
-//    loc_p = loc_changed[0];
-//    ans_p = ans_changed[0];
-//    for (int i = 0; i<92; i++)
-//    {
-//        if (count_zeros[i][0] == max_zeros )
-//        {
-//            optimal_solution_count++;
+    printf("\n max_zeros = %d \n",max_zeros);
+    /******optimal solution round one************/
+    printf("\nzeros matched\n");
+
+    loc_p = loc_changed[0];
+    ans_p = ans_changed[0];
+    for (int i = 0; i<92; i++)
+    {
+        if ( count_zeros[i][0] == max_zeros )
+        {
+            optimal_solution_count++;
 //            optimal_solution[optimal_solution_count-1] = i;
-//            printf("%d ",i+1);
-//            printf("\nmatched loc is\n");
-//            for (int j = 0;j<8; j++)
-//            {
-//                printf("%d   %d\n",loc[j],ans[i][j]);//打印匹配结果
-//            }
-//
-//
-//            for (int j = 0;j< 2*count_zeros[i][0];j++)
-//            {
-//                printf("%d ",count_zeros[i][j+1]); //打印匹配位置
-//            }
-//            printf("\n");
-//
-//            count_zeros_p = count_zeros[i];
-//            count_zeros_p += 1;  //count_zeros_p指向count_zeros[1]
-//            for (int j = 0; j < 8; j++)
-//            {
-//                if (j == *count_zeros_p)
-//                    count_zeros_p += 2;
-//                else
-//                {
-//                    *loc_p = loc[j]; //loc_changed
-//                    loc_p++;
-//                }
-//            }
-//            loc_p += max_zeros ;
-//
-//            count_zeros_p = count_zeros[i];
-//            count_zeros_p += 2;  //count_zeros_p指向count_zeros[2]
-//            for (int j = 0; j < 8; j++)
-//            {
-//                if (j == *count_zeros_p)
-//                    count_zeros_p += 2;
-//                else
-//                {
-//                    *ans_p = ans[i][j]; //ans_changed
-//                    ans_p++;
-//                }
-//            }
-//            ans_p += max_zeros ;
-//        }
-//    }
-//
-//
-//    printf("\n optimal_solution_count = %d \n",optimal_solution_count);//打印最优解的总数
-//
-//    loc_p = loc_changed[0];
-//    ans_p = ans_changed[0];
-//    for ( int i = 0; i < optimal_solution_count; i++, loc_p += max_zeros, ans_p += max_zeros)
-//    {
-//        printf("solution %d is :\n",i+1);
-//        for ( int j = 0; j < (8 - max_zeros); j++,loc_p++,ans_p++)
-//        {
-//            printf("%d  %d\n",*loc_p, *ans_p);
-//        }
-//        printf("\n\n");
-//    }
-//
-//    printf("sizeof loc_changed is %d\n",sizeof(loc_changed[0])/sizeof(int));
-//
+            printf("%d ",i+1);
+            printf("\nmatched loc is\n");
+            for (int j = 0;j<8; j++)
+                printf("%d   %d\n",loc[j],ans[i][j]);//打印匹配结果
 
+            for (int j = 0;j< 2*count_zeros[i][0];j++)
+            {
+                printf("%d ",count_zeros[i][j+1]); //打印匹配位置
+            }
+            printf("\n");
 
+            count_zeros_p = count_zeros[i];
+            count_zeros_p += 1;  //count_zeros_p指向count_zeros[1]
+            for (int j = 0; j < 8; j++)
+            {
+                if (j == *count_zeros_p)
+                    count_zeros_p += 2;
+                else
+                {
+                    *loc_p = loc[j]; //loc_changed
+                    loc_p++;
+                }
+            }
+            loc_p += max_zeros ;
 
+            count_zeros_p = count_zeros[i];
+            count_zeros_p += 2;  //count_zeros_p指向count_zeros[2]
+            for (int j = 0; j < 8; j++)
+            {
+                if (j == *count_zeros_p)
+                    count_zeros_p += 2;
+                else
+                {
+                    *ans_p = ans[i][j]; //ans_changed
+                    ans_p++;
+                }
+            }
+            ans_p += max_zeros ;
+        }
+    }
+
+    printf("\noptimal_solution_count = %d \n",optimal_solution_count);//打印最优解的总数
+
+    loc_p = loc_changed[0];
+    ans_p = ans_changed[0];
+
+    for ( int i = 0; i < optimal_solution_count; i++, loc_p += max_zeros, ans_p += max_zeros)
+    {
+        printf("solution %d is :\n",i+1);
+        for ( int j = 0; j < (8 - max_zeros); j++,loc_p++,ans_p++)
+        {
+            printf("%d  %d\n",*loc_p, *ans_p);
+        }
+        printf("\n\n");
+    }
 
     return 0;
 }
