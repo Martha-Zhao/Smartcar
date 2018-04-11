37a712c9cdf1490cf8f9d93dd2ef0e270ece2170
Martha-Zhao <34574457+Martha-Zhao@users.noreply.github.com>
2018-04-11 15:53:50 +0800

    Create shoutest_route.h

---
+++ prog/shoutest route/shoutest_route.h
@@ -0,0 +1,13 @@
+#ifndef __SHOUTEST_ROUTE_H__
+#define __SHOUTEST_ROUTE_H__
+
+#include <stdlib.h>
+
+
+
+void sot (int *dis_p, int *loc_p, int length);
+
+int * best_ans(int *loc_p, int *ans_p, int length);
+
+
+#endif // __SHOUTEST_ROUTE_H__
