2d2dc1529969821eb0153ed7a8af4c63b62e90c1
Martha-Zhao <34574457+Martha-Zhao@users.noreply.github.com>
2018-04-12 15:50:19 +0800

    Update shoutest_route.h

--- prog/find answer/shoutest_route.h
+++ prog/find answer/shoutest_route.h
@@ -4,19 +4,19 @@
 #include <stdlib.h>
 
 //int solution[8][8];
-
+//返回结构体
 struct find_route
 {
-    int *p;
-    int min_sum;
-    int turn;
+    int *p; //solution数组的地址
+    int min_sum; //最优解路径长度
+    int turn; //最优解顺序
 };
 
 struct find_route f_r;
 
-void sot (int *dis_p, int *loc_p, int length);
+void sot (int *dis_p, int *loc_p, int length); //排序
 
-struct  find_route *best_ans(int *loc_p, int *ans_p, int length);
+struct  find_route *best_ans(int *loc_p, int *ans_p, int length); //最优路径寻找
 
 
 #endif // __SHOUTEST_ROUTE_H__
