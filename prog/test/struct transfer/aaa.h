50d285cbd477ceed2590ad72c182af8197f51b65
Martha-Zhao <34574457+Martha-Zhao@users.noreply.github.com>
2018-04-12 10:40:53 +0800

    Create aaa.h

---
+++ prog/test/struct transfer/aaa.h
@@ -0,0 +1,14 @@
+#ifndef __AAA_H__
+#define __AAA_H__
+
+struct student
+ {
+    int *p;
+    int min_sum;
+ };
+
+ //int dis [8][8] = {0};
+struct student b;
+struct  student *fun (void);
+
+#endif // __STD_H__
