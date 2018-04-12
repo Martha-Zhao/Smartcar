e925515c0308f8c23b5f6eb79f77534ef33d1a08
Martha-Zhao <34574457+Martha-Zhao@users.noreply.github.com>
2018-04-12 10:41:13 +0800

    Create aaa.c

---
+++ prog/test/struct transfer/aaa.c
@@ -0,0 +1,11 @@
+#include "aaa.h"
+#include <stdio.h>
+
+struct  student *fun(void)
+{
+    int dis[2][2] = {{11,12},{13,14}};
+    b.p = dis[0];
+    b.min_sum = dis[0][0];
+    printf("%d\n",*(b.p));
+    return &b;
+}
